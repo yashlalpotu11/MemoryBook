00d5c7046cf816827a99642b5f7952057a6e0989
YASH  LALPOTU <yashrlalpotu30@gmail.com>
2021-11-27 16:22:28 +0530

    make it better

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require('path');
 
 
 const app = express();
@@ -20,13 +21,14 @@ app.use('/posts', require('./Routes/postRouter'));
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
-    const path = require('path');
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
-app.listen((process.env.PORT || 5000), () => {
-  console.log(`Server is running on port 5000...`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}...`);
 });
 
