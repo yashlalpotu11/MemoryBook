a52bb7a85f4561f45c1803ffedd47ffbc6b3c94e
YASH  LALPOTU <yashrlalpotu30@gmail.com>
2021-11-27 15:31:25 +0530

    deplyoment

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ app.use('/posts', require('./Routes/postRouter'));
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
-    // const path = require('path');
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    // });
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 app.listen(port, () => {
